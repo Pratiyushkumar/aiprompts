447d60173b9e5af1392399be35308ebffbcb3126
Pratiyush Kumar <pratiyush.inv@gmail.com>
2024-03-02 21:42:41 +0530

    navbar created for desktop and mobile view

--- components/Nav.jsx
+++ components/Nav.jsx
@@ -6,6 +6,18 @@ import Image from 'next/image';
 import { signIn, signOut, useSession, getProviders } from 'next-auth/react';
 
 const Nav = () => {
+  const isUserLoggedIn = true;
+
+  const [providers, setProviders] = useState(null);
+  const [toggleDropdown, setToggleDropdown] = useState(false);
+
+  useEffect(() => {
+    (async () => {
+      const res = await getProviders();
+      setProviders(res);
+    })();
+  }, []);
+
   return (
     <nav className='flex-between w-full mb-16 pt-3'>
       <Link href='/' className='flex gap-2 flex-center'>
@@ -18,6 +30,104 @@ const Nav = () => {
         />
         <p className='logo_text'>AIPrompts</p>
       </Link>
+      {/* Desktop Navigation */}
+      <div className='sm:flex hidden'>
+        {isUserLoggedIn ? (
+          <div className='flex gap-3 md:gap-5'>
+            <Link href='/create-prompt' className='black_btn'>
+              Create Posts
+            </Link>
+
+            <button type='button' className='outline_btn' onClick={signOut}>
+              Sign out
+            </button>
+
+            <Link href='/profile'>
+              <Image
+                src='/assets/images/logo.svg'
+                alt='profile image'
+                width={37}
+                height={37}
+              />
+            </Link>
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <>
+                  <button
+                    type='button'
+                    className='black_btn'
+                    key={provider.name}
+                    onClick={() => signIn(provider.id)}
+                  >
+                    Sign In
+                  </button>
+                </>
+              ))}
+          </>
+        )}
+      </div>
+
+      {/* Mobile Navigation */}
+      <div className='sm:hidden flex relative'>
+        {isUserLoggedIn ? (
+          <div className='flex'>
+            <Image
+              src='/assets/images/logo.svg'
+              alt='profile image'
+              width={37}
+              height={37}
+              onClick={() => setToggleDropdown((prev) => !prev)}
+            />
+            {toggleDropdown && (
+              <div className='dropdown'>
+                <Link
+                  href='/profile'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  My Profile
+                </Link>
+                <Link
+                  href='/create-prompt'
+                  className='dropdown_link'
+                  onClick={() => setToggleDropdown(false)}
+                >
+                  Create Prompt
+                </Link>
+                <button
+                  type='button'
+                  onClick={() => {
+                    setToggleDropdown(false);
+                    signOut();
+                  }}
+                  className='mt-5 w-full black_btn'
+                >
+                  Sign Out
+                </button>
+              </div>
+            )}
+          </div>
+        ) : (
+          <>
+            {providers &&
+              Object.values(providers).map((provider) => (
+                <>
+                  <button
+                    type='button'
+                    className='black_btn'
+                    key={provider.name}
+                    onClick={() => signIn(provider.id)}
+                  >
+                    Sign In
+                  </button>
+                </>
+              ))}
+          </>
+        )}
+      </div>
     </nav>
   );
 };
