d4d773e91cc6f6f07f230e8b6279fa60c8a2500a
Pratiyush Kumar <pratiyush.inv@gmail.com>
2024-03-10 21:07:51 +0530

    my profile feature completed

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -1,5 +1,39 @@
-const Profile = () => {
-  return <div>Profile</div>;
+'use client';
+
+import { useState, useEffect } from 'react';
+import { useSession } from 'next-auth/react';
+import { useRouter } from 'next/router';
+import Profile from '@components/Profile';
+
+const MyProfile = () => {
+  //   const router = useRouter();
+  const { data: session } = useSession();
+  const [myPosts, setMyPosts] = useState([]);
+
+  useEffect(() => {
+    const fetchPosts = async () => {
+      const response = await fetch(`/api/users/${session?.user.id}/posts`);
+      const data = await response.json();
+      setMyPosts(data);
+    };
+    if (session?.user.id) fetchPosts();
+  }, [session?.user.id]);
+
+  const handleEdit = () => {
+    console.log('Edit function called');
+  };
+  const handleDelete = async () => {
+    console.log('Delete function called');
+  };
+  return (
+    <Profile
+      name='My'
+      desc='Welcome to your personalized profile page. Share your exceptional prompts and inspire others with the power of your imagination'
+      data={myPosts}
+      handleEdit={handleEdit}
+      handleDelete={handleDelete}
+    />
+  );
 };
 
-export default Profile;
+export default MyProfile;
